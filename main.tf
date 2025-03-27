1aee7ca03958ee3fa011ce5dc57670607e80458d
Kirshi Yin <70095657+kirshiyin89@users.noreply.github.com>
2025-03-27 15:30:26 +0100

    Update main.tf

--- main.tf
+++ main.tf
@@ -2,27 +2,29 @@ terraform {
   required_providers {
     docker = {
       source  = "kreuzwerker/docker"
-      version = "3.0.2"
+      version = "~> 3.0"
     }
   }
 }
 
 provider "docker" {}
 
-resource "docker_image" "nginx" {
-  # Security Violation 1: Using an outdated and potentially vulnerable image
-  name = "nginx:1.16.0"  
+resource "docker_image" "vulnerable_nginx" {
+  name = "nginx:1.16.0"  # Deliberately using an old, vulnerable image
   keep_locally = false
 }
 
-resource "docker_container" "nginx" {
-  name  = "nginx-container"
-  image = docker_image.nginx.image_id
+resource "docker_container" "insecure_container" {
+  name  = "insecure-nginx"
+  image = docker_image.vulnerable_nginx.image_id
 
   ports {
     internal = 80
     external = 8080
-    # Security Violation 2: Exposing port to all interfaces (0.0.0.0)
+    # Security risk: exposing to all interfaces
     ip = "0.0.0.0"
   }
+
+  # Intentional security misconfigurations
+  privileged = true  # Extremely risky - gives full host access
 }
