5a71d73fc96e1ce200c89bf7812e55ac68917172
Kirshi Yin <70095657+kirshiyin89@users.noreply.github.com>
2025-03-27 15:07:55 +0100

    Update main.tf

--- main.tf
+++ main.tf
@@ -10,18 +10,19 @@ terraform {
 provider "docker" {}
 
 resource "docker_image" "nginx" {
-  name = "nginx:latest"
+  # 🚨 Security Violation: Using an old, insecure image
+  name = "nginx:1.16.0"
 }
 
 resource "docker_container" "nginx" {
   name  = "nginx"
   image = docker_image.nginx.image_id
+
   ports {
     internal = 80
     external = 8080
+
+    # 🚨 Security Violation: Exposing to all networks (0.0.0.0)
+    ip = "0.0.0.0"
   }
-
-  # 🚨 Security Violation: Running as root (Checkov Rule: CKV_DOCKER_8)
-  privileged = true
 }
-
