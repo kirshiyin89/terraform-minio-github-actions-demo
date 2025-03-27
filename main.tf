d869f62da0eb840d400fec02254e4377bfe068a4
Kirshi Yin <70095657+kirshiyin89@users.noreply.github.com>
2025-03-27 15:44:55 +0100

    Update main.tf

--- main.tf
+++ main.tf
@@ -1,8 +1,8 @@
 terraform {
   required_providers {
     aws = {
-      source  = "hashicorp/aws"
-      version = "~> 5.0"
+      source = "hashicorp/aws"
+      version = "5.92.0"
     }
   }
 }
@@ -11,29 +11,20 @@ provider "aws" {
   region = "us-west-2"
 }
 
+resource "aws_s3_bucket" "example" {
+  bucket = "my-tf-example-bucket"
+}
+
+resource "aws_s3_bucket_ownership_controls" "example" {
+  bucket = aws_s3_bucket.example.id
+  rule {
+    object_ownership = "BucketOwnerPreferred"
+  }
+}
+
 resource "aws_s3_bucket_acl" "example" {
   depends_on = [aws_s3_bucket_ownership_controls.example]
 
   bucket = aws_s3_bucket.example.id
   acl    = "public"
-}
-
-resource "aws_security_group" "example" {
-  name        = "allow-all"
-  description = "Allow all inbound and outbound traffic"
-
-  # Deliberately open security group
-  ingress {
-    from_port   = 0
-    to_port     = 0
-    protocol    = "-1"
-    cidr_blocks = ["0.0.0.0/0"]
-  }
-
-  egress {
-    from_port   = 0
-    to_port     = 0
-    protocol    = "-1"
-    cidr_blocks = ["0.0.0.0/0"]
-  }
 }
