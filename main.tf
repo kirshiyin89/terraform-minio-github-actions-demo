d873baeb7a01a990d8bece13e7693df21f90b84e
Kirshi Yin <70095657+kirshiyin89@users.noreply.github.com>
2025-03-27 13:41:08 +0100

    Create main.tf

---
+++ main.tf
@@ -0,0 +1,22 @@
+terraform {
+  required_providers {
+    docker = {
+      source = "kreuzwerker/docker"
+      version = "3.0.2"
+    }
+  }
+}
+
+resource "docker_container" "minio" {
+  name  = "minio"
+  image = "minio/minio"
+  ports {
+    internal = 9000
+    external = 9000
+  }
+  env = [
+    "MINIO_ROOT_USER=admin",
+    "MINIO_ROOT_PASSWORD=supersecret"
+  ]
+  command = ["server", "/data"]
+}
